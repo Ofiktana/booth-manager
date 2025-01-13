62c813bff40e0ee7499ba830b7071444852f290a
Tana Ofik <ofik.tana@gmail.com>
2025-01-13 13:21:00 +0100

    Initial hello world app

---
+++ booth-manager-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const style = {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: '5rem'
+  }
+
+  return (
+    <>
+      <div style={style}>Hello World</div>
+    </>
+  )
+}
+
+export default App
